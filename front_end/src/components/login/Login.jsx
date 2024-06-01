f7026e2b4a11ced9568000ac9d708c7bb124d74a
Victor Dias <victordiaspereira53@gmail.com>
2024-05-31 22:57:04 -0300

    Dados enviando para o back_end

--- front_end/src/components/login/Login.jsx
+++ front_end/src/components/login/Login.jsx
@@ -1,49 +1,34 @@
 import { FaUser, FaLock } from "react-icons/fa";
-import { useState } from "react";
 import axios from "axios";
 
 import "./login.css";
 
-axios.get('http://127.0.0.1:8000/api/users')
-    .then((response) => {
-        console.log(response)
-    })
+const backendUrl = 'http://127.0.0.1:8000/api';
+
 
 const Login = () => {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        axios.post(`http://127.0.0.1:8000/api/login`, { username, password })
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-            })
-    }
-
     return (
         <div className="container">
-            <form onSubmit={handleSubmit} method="POST">
+            <form onSubmit={handleSubmit}>
                 <img src="/images/ifnmg.png" alt="" width="100" />
                 <hr />
                 <p>Preencha os campos para entrar</p>
 
                 <div className="input-field">
                     <FaUser className="icon" />
-                    <input type="text" placeholder="Digite seu usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" name='user' placeholder="Digite seu usuario" />
                 </div >
                 <div className="input-field">
                     <FaLock className="icon" />
-                    <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" name='password' placeholder="Senha" />
                 </div>
                 <div>
                     <input type="checkbox" />
                     <label> Permanecer Conectado</label>
                 </div>
                 <button type="submit">Entrar</button>
+                
                 <hr />
                 <div className="final">
                     <p>© 2024 Gabriel de Sousa e Victor Dias</p>
@@ -54,3 +39,24 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    try {
+        const user = event.target.user.value;
+        const password = event.target.password.value;
+
+        axios.post(`${backendUrl}/login`, { user, password })
+            .then(response => {
+                console.log('Dados enviados com sucesso');
+                if (response.data.role === 'ADM') {
+                    window.location.href = 'http://127.0.0.1:8000/api/users';
+                }
+            })
+            
+    } catch (error) {
+        console.log(error);
+    }
+}
